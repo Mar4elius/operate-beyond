d694ff0b14bc9f96a8d4d16c2329f93fae428809
Mar4elius <marauman@gmail.com>
2021-07-21 20:07:48 -0600

    Create migrations and models

---
+++ app/Models/Book.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    /**
+     * Book can belong only to ONE Author
+     * Get book author.
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    /**
+     * Get libraries where book is available
+     */
+    public function libraries(): BelongsToMany
+    {
+        return $this->belongsToMany(Library::class);
+    }
+}
---
+++ database/migrations/2021_07_22_013108_create_authors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->enum('genre', ['Fiction', 'Novel', 'Science Fiction', 'Narrative', 'Mystery']); // has more genres, but that should be enough for testing
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
